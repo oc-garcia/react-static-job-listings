25e7d3333591fe46907e165cd0482177b80faa76
oc-garcia <octaviogarcia1988@gmail.com>
2023-02-20 19:56:36 -0300

    Finished styling

--- src/Components/JobCard/index.jsx
+++ src/Components/JobCard/index.jsx
@@ -7,28 +7,35 @@ export default function JobCard() {
     <section className={styles.cardContainer}>
       {jobs.map(job => 
       <div className={styles.card}>
-        <img className={styles.logo} src={`${job.logo}`} alt='Company Logo'/>
-        <div className={styles.jobSpecsContainer}>
-          <div className={styles.jobInformationContainer}>
-            <p className={styles.companyName}>{job.company}</p>
-            {job.new && <p className={styles.new}>NEW!</p>}
-            {job.featured && <p className={styles.featured}>FEATURED</p>}
-          </div>
-          <div className={styles.jobNameContainer}>
-            <p className={styles.jobPosition}>{job.position}</p>
-          </div>
-          <div className={styles.jobDescriptonContainer}>
-            <p>{job.postedAt}</p>
-            <p>{job.contract}</p>
-            <p>{job.location}</p>
+        <div className={styles.cardSubContainer1}>
+          <img className={styles.logo} src={`${job.logo}`} alt='Company Logo'/>
+          <div className={styles.jobSpecsContainer}>
+            <div className={styles.jobInformationContainer}>
+              <p className={styles.companyName}>{job.company}</p>
+              {job.new && <p className={styles.new}>NEW!</p>}
+              {job.featured && <p className={styles.featured}>FEATURED</p>}
+            </div>
+            <div className={styles.jobNameContainer}>
+              <p className={styles.jobPosition}>{job.position}</p>
+            </div>
+            <div className={styles.jobDescriptonContainer}>
+              <p className={styles.postedAt}>{job.postedAt}</p>
+              <div className={styles.dot}></div>
+              <p className={styles.contract}>{job.contract}</p>
+              <div className={styles.dot}></div>
+              <p className={styles.location}>{job.location}</p>
+            </div>
           </div>
         </div>
-        <ul>
-          {job.languages.map((lang) => <li>{lang}</li>)}
-          {job.tools.map((tool) => <li>{tool}</li>)}
-        </ul>
+        <div className={styles.cardSubContainer2}>
+          <ul>
+            {job.languages.map((lang) => <li>{lang}</li>)}
+            {job.tools.map((tool) => <li>{tool}</li>)}
+          </ul>
+        </div>
       </div>
       )}
+      
     </section>
   )
 }
